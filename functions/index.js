b85d0cec8c24155a8bba28641f31682f56481027
Hafez <a.hafez852@gmail.com>
2018-09-14 21:23:19 +0200

    testing post data

--- functions/index.js
+++ functions/index.js
@@ -3,8 +3,17 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+
+console.log('this runs no matter what!');
+console.log('hello from the other side');
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
   console.log('I can see this!');
   console.log(functions.config().hello.there);
   response.send('Hello from Firebase!');
+});
+
+
+exports.postData = functions.https.onRequest((req, res) => {
+  console.log(req.body);
 });
